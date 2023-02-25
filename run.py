474a4d3546917f69187ca3c03079a0d4b1251900
Rudal-XD <106883384+Rudal-XD@users.noreply.github.com>
2023-02-25 09:02:30 +0700

    Update run.py

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-import requests,json,bs4,time,re,os
+import os
 import threading
 from rich.table import Table as me
 from rich.console import Console as sol
@@ -10,29 +10,25 @@ from rich import print as cetak
 from rich.markdown import Markdown as mark
 from rich.columns import Columns as col
 
+
 try:
-    	IP = requests.get('https://api.ipify.org/').text
-    	sky='# YOUR ALAMAT IP : %s'%(IP)
-    	sky2=mark(sky,style='green')
-    	sol().print(sky2,style='cyan')
-    	try:
-    		ski='# 1.OPEN'
-    		ski2=mark(ski,style='green')
-    		sol().print(ski2,style='cyan')
-    		try:
-    			sue='# 2.KELUAR'
-    			sue2=mark(sue,style='green')
-    			sol().print(sue2,style='cyan')
-    		except IOError:
-    			print()
-    	except IOError:
-    		print('gae lo')
-except IOError:
-    	print()
-    	
-    	
-usna = input('nomor:')
-if usna == '1':
-	print('succes')
-elif usna == '2':
-	exit()
+	import rich
+	sue='# module rich telah terinstall...'
+	sui=mark(sue,style='green')
+	sol().print(sui,style='cyan')
+except ImportError:
+	sup='# module rich belum terinstall...'
+	suo=mark(sup,style='green')
+	sol().print(suo,style='cyan')
+	os.system('pip install rich')
+	
+try:
+	import ruby
+	sul='# module ruby belum terinstall...'
+	syu=mark(sul,style='green')
+	sol().print(syu,style='cyan')
+except ImportError:
+	suq='# module ruby belum terinstall...'
+	suh=mark(suq,style='green')
+	sol().print(suh,style='cyan')
+	os.system('pkg install ruby -y')
