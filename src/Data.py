bf3a9843c01869cc8d7747d075a7ee210f7a9092
Rudal-XD <106883384+Rudal-XD@users.noreply.github.com>
2023-02-28 16:12:44 +0700

    Update Data.py

--- src/Data.py
+++ src/Data.py
@@ -1,1 +1,32 @@
+import os,requests
 
+def update():
+  os.system('git pull')
+def clear():
+  os.system('clear')
+ def login():
+  menu()
+  
+  banner=('OPEN WAR')
+  
+class menu:
+  print(f"""{banner}
+        1.open
+        2.keluar""")
+  usna = input('pilih nomor:')
+  if usna == '1':
+    try:
+      open=requests.get('https://google.com')
+    except IOError:
+      print('masuk error')
+      
+  elif usna == '2':
+    exit('thank bro')
+  else:
+    print('masukan nomor yang benar bro')
+    
+    
+if __name__=='__main__':
+  update()
+  clear()
+  login()
