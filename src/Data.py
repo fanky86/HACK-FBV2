f807be945d41b3685bbb09f0d716bc5a4083dd56
Rudal-XD <106883384+Rudal-XD@users.noreply.github.com>
2023-03-01 15:44:54 +0700

    Update Data.py

--- src/Data.py
+++ src/Data.py
@@ -1,4 +1,24 @@
-import os,requests
+import requests,bs4,json,os,sys,random,datetime,time,re
+
+import threading
+
+from rich.table import Table as me
+
+from rich.console import Console as sol
+
+from bs4 import BeautifulSoup as parser
+
+from concurrent.futures import ThreadPoolExecutor as tred
+
+from rich.console import Group as gp
+
+from rich.panel import Panel as nel
+
+from rich import print as cetak
+
+from rich.markdown import Markdown as mark
+
+from rich.columns import Columns as col
 
 def update():
   os.system('git pull')
