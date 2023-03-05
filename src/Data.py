d515d151b6183489c17c4fbfae06e4b87a82c3bb
Rudal-XD <106883384+Rudal-XD@users.noreply.github.com>
2023-03-05 15:02:05 +0700

    Update Data.py

--- src/Data.py
+++ src/Data.py
@@ -97,7 +97,7 @@ class login:
 	elif coi in ['2','02']:
 		sky='[1] Install module\n[2] HACK'
 		lop=nel(sky,style='green')
-		cetak=(nel(lop,title='MENU')
+		cetak=(nel(lop,title='MENU'))
 		usna == input('[?] pilih :')
 		if usna in ['1']:
 		       print('wlwkwkwk')
