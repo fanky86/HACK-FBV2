7b9da7161a3caafc30c4dccb4a8dee43ae860837
Rudal-XD <106883384+Rudal-XD@users.noreply.github.com>
2023-03-05 22:32:39 +0700

    Update Data.py

--- src/Data.py
+++ src/Data.py
@@ -100,7 +100,7 @@ class login:
 		sky='[1] Doa Pemancar aura\n[2] HACK\n[3] BACK TO MENU'
 		lop=nel(sky,style='green')
 		cetak(nel(lop,style='cyan',title='MENU'))
-		usna = input('[?] pilih :')
+		usna = input('{u}[?] pilih :')
 		if usna in ['1']:
 			oss='Iż qāla yụsufu li`abīhi yā abati innī ra`aitu aḥada asyara kaukabaw wasy-syamsa wal-qamara ra`aituhum lī sājidīn\n\nArtinya: (Ingatlah), ketika Yusuf berkata kepada ayahnya: "Wahai ayahku, sesungguhnya aku bermimpi melihat sebelas bintang, matahari dan bulan; kulihat semuanya sujud kepadaku".'
 			oos=nel(oss,style='green')
