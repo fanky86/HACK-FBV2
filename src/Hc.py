b71d07b7805c0301907e9d8b17da1df1c876b62a
Rudal-XD <106883384+Rudal-XD@users.noreply.github.com>
2023-03-06 10:08:43 +0700

    Update Hc.py

--- src/Hc.py
+++ src/Hc.py
@@ -1,3 +1,141 @@
-import os
+import requests,bs4,json,os,sys,random,datetime,time,re
+import threading
 
-print('succes')
+from rich.table import Table as me
+
+from rich.console import Console as sol
+
+from bs4 import BeautifulSoup as parser
+
+from concurrent.futures import ThreadPoolExecutor as tred
+
+from rich.console import Group as gp
+
+from rich.panel import Panel as nel
+
+from rich import print as cetak
+
+from rich.markdown import Markdown as mark
+
+from rich.columns import Columns as col
+
+x = '\33[m' # DEFAULT
+
+k = '\033[93m' # KUNING +
+
+h = '\x1b[1;92m' # HIJAU +
+
+hh = '\033[32m' # HIJAU -
+
+u = '\033[95m' # UNGU
+
+kk = '\033[33m' # KUNING -
+
+b = '\33[1;96m' # BIRU -
+
+p = '\x1b[0;34m' # BIRU +
+
+P = '\033[0;00m'
+
+J = '\033[0;33m'
+
+S = '\033[0;00m'
+
+N = '\x1b[0m'
+
+I ='\033[0;32m'
+
+C ='\033[0;36m'
+
+M ='\033[0;31m'
+
+U ='\033[0;35m'
+
+K ='\033[0;33m'
+
+P='\033[00m'
+
+h='\033[0;90m'
+
+Q="\033[00m"
+
+kk='\033[0;32m'
+
+ff='\033[0;36m'
+
+G='\033[0;36m'
+
+p='\033[00m'
+
+h='\033[0;90m'
+
+Q="\033[00m"
+
+I='\033[0;32m'
+
+II='\033[0;36m'
+
+m='\033[0;31m'
+
+O ='\033[0;33m'
+
+H='\033[0;33m'
+
+b = '\033[0;36m'
+
+war = "[•]"
+
+B = random.choice([U,I,K,b,M])
+def banner():
+	print('''
+		\x1b[1;92m_______           ______   _______  _                 ______  
+       (  ____ )|\     /|(  __  \ (  ___  )( \      |\     /|(  __  \ 
+       | (    )|| )   ( || (  \  )| (   ) || (      ( \   / )| (  \  )
+       | (____)|| |   | || |   ) || (___) || | _____ \ (_) / | |   ) |
+       |     __)| |   | || |   | ||  ___  || |(_____) ) _ (  | |   | |
+       | (\ (   | |   | || |   ) || (   ) || |       / ( ) \ | |   ) |
+       | ) \ \__| (___) || (__/  )| )   ( || (____/\( /   \ )| (__/  )
+       |/   \__/(_______)(______/ |/     \|(_______/|/     \|(______/ 
+                                                               
+───────────────────────────────────────────────────────
+ [\x1b[1;96m+%s] Author   : Rudal-XD
+ [\x1b[1;96m+%s] Github   : https://github.com/Rudal-XD
+ [\x1b[1;96m+%s] Facebook : Fanky
+───────────────────────────────────────────────────────''')
+def clear():
+	os.system('clear')
+def update():
+	os.system('git pull')
+	
+	
+def login():
+	banner()
+	sky = '# MASUKAN TOKEN FACEBOOK,SARAN PAKAI AKUN TUMBAL'
+	sky2 = mark(sky, style='green')
+	sol().print(sky2, style='cyan')
+	panda = input(M+'['+hh+'•'+M+'] Token Fb EAA : %s'%(k))
+	akun=open('.token.x','w').write(panda)
+	try:
+		tes = requests.get('https://graph.facebook.com/me?access_token='+panda)
+		tes3 = json.loads(tes.text)['id']
+		sue = '# nice Login berhasil'
+		suu = mark(sue, style='green')
+		sol().print(suu, style='cyan')
+		time.sleep(2)
+		menu()
+	except KeyError:
+		sue = '# Login Gagal, Cek token'
+		suu = mark(sue, style='red')
+		sol().print(suu, style='cyan')
+		time.sleep(2)
+		os.system('clear')
+		login()
+	except requests.exceptions.ConnectionError:
+		li = '# KONEKSI INTERNET BERMASALAH, PERIKSA & COBA LAGI'
+		lo = mark(li, style='red')
+		sol().print(lo, style='cyan')
+		menu()
+		
+		
+if __name__=='__main__':
+	login()
