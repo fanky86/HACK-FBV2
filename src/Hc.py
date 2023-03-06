91d06c9e4744e59665bf6642f716977b0dd2c5eb
Rudal-XD <106883384+Rudal-XD@users.noreply.github.com>
2023-03-06 12:17:03 +0700

    Update Hc.py

--- src/Hc.py
+++ src/Hc.py
@@ -10,11 +10,11 @@ from rich import print as cetak
 from rich.markdown import Markdown as mark
 from rich.columns import Columns as col
 
-
+os sy
 
 try:
 	oss='Latin : “Alloohu laa ilaaha illaa huwal hayyul qoyyuum, laa ta’khudzuhuu sinatuw walaa naum. Lahuu maa fissamaawaati wa maa fil ardli man dzal ladzii yasyfa’u ‘indahuu illaa biidznih, ya’lamu maa baina aidiihim wamaa kholfahum wa laa yuhiithuuna bisyai’im min ‘ilmihii illaa bimaa syaa’ wasi’a kursiyyuhus samaawaati wal ardlo walaa ya’uuduhuu hifdhuhumaa wahuwal ‘aliyyul ‘adhiim.”\n\nArtinya : "Allah, tidak ada Tuhan (yang berhak disembah) melainkan Dia Yang Hidup kekal lagi terus menerus mengurus (makhluk-Nya); tidak mengantuk dan tidak tidur. Kepunyaan-Nya apa yang di langit dan di bumi. Tiada yang dapat memberi syafaat di sisi Allah tanpa izin-Nya.Allah mengetahui apa-apa yang di hadapan mereka dan di belakang mereka, dan mereka tidak mengetahui apa-apa dari ilmu Allah melainkan apa yang dikehendaki-Nya. Kursi Allah meliputi langit dan bumi. Dan Allah tidak merasa berat memelihara keduanya, dan Allah Maha Tinggi lagi Maha Besar"\n(QS. Al Baqarah: 255)'
 	opp=nel(oss,style='green')
-	cetak(nel(opp,title='*「 Ayat Kursi 」*'))
+	cetak(nel(opp,style='cyan',title='*「 Ayat Kursi 」*'))
 except KeyError:
 	print()
