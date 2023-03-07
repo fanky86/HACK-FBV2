0710d3108a70d63e8bf40d069b712703d213aeb0
Rudal-XD <106883384+Rudal-XD@users.noreply.github.com>
2023-03-07 08:54:48 +0700

    Update Hc.py

--- src/Hc.py
+++ src/Hc.py
@@ -17,6 +17,6 @@ try:
 	opp=nel(oss,style='green')
 	cetak(nel(opp,style='cyan',title='*「 Ayat Kursi 」*'))
 	enter=input('tekan enter')
-	Ket= from src import Data .write(enter)
+	os.system('python run.py')
 except KeyError:
 	print()
