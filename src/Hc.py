95bce1a4ca6b3cb1754f83a82aa5ab838fa07bb1
Rudal-XD <106883384+Rudal-XD@users.noreply.github.com>
2023-03-07 08:57:45 +0700

    Update Hc.py

--- src/Hc.py
+++ src/Hc.py
@@ -16,7 +16,7 @@ try:
 	oss='Latin : “Alloohu laa ilaaha illaa huwal hayyul qoyyuum, laa ta’khudzuhuu sinatuw walaa naum. Lahuu maa fissamaawaati wa maa fil ardli man dzal ladzii yasyfa’u ‘indahuu illaa biidznih, ya’lamu maa baina aidiihim wamaa kholfahum wa laa yuhiithuuna bisyai’im min ‘ilmihii illaa bimaa syaa’ wasi’a kursiyyuhus samaawaati wal ardlo walaa ya’uuduhuu hifdhuhumaa wahuwal ‘aliyyul ‘adhiim.”\n\nArtinya : "Allah, tidak ada Tuhan (yang berhak disembah) melainkan Dia Yang Hidup kekal lagi terus menerus mengurus (makhluk-Nya); tidak mengantuk dan tidak tidur. Kepunyaan-Nya apa yang di langit dan di bumi. Tiada yang dapat memberi syafaat di sisi Allah tanpa izin-Nya.Allah mengetahui apa-apa yang di hadapan mereka dan di belakang mereka, dan mereka tidak mengetahui apa-apa dari ilmu Allah melainkan apa yang dikehendaki-Nya. Kursi Allah meliputi langit dan bumi. Dan Allah tidak merasa berat memelihara keduanya, dan Allah Maha Tinggi lagi Maha Besar"\n(QS. Al Baqarah: 255)'
 	opp=nel(oss,style='green')
 	cetak(nel(opp,style='cyan',title='*「 Ayat Kursi 」*'))
-	enter=input('\x1b[1;93m* tekan enter untuk kembali ke menu awal *')
+	enter=input('\x1b[1;93m* tekan \x1b[1;92menter \x1b[1;93muntuk kembali ke menu awal *')
 	os.system('python run.py')
 except KeyError:
 	print()
